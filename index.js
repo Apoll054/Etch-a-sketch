af2b0bd2be88391366dcf1ff8bb28680d55298b6
Anupam Panthi <anupampanthi12@gmail.com>
2026-06-17 21:14:46 -0500

    added coloring functionality
    added eventListener to detect mouseover and fill the background black

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const container= document.querySelector("#container")
 for(let i=0; i<256; i++){
     const square=document.createElement('div');
     square.classList.add("square");
+    square.addEventListener('mouseenter',(e)=>{
+        e.target.style.backgroundColor="black";
+    })
     container.append(square);
 }
