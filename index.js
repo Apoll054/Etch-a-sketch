35fa24e2a609df728d79c7dfe2a27795a1f2568a
Anupam Panthi <anupampanthi12@gmail.com>
2026-06-20 03:22:07 -0500

    Added random color and opacity functionality in js
    randomized the color the square is given and added gradual increase in opacity

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const container= document.querySelector("#container")
 const resizeBtn=document.querySelector('#resize-btn')
 const resetBtn=document.querySelector('#reset-btn')
+const colorArray=['#0E1848', '#485699','#6D8AB1']
 
 
 
@@ -26,9 +27,29 @@ function createGrid(size){
     for(let i=0; i<totalSquares; i++){
         const square=document.createElement('div');
         square.classList.add("square");
+        square.dataset.interactions = 0;
         square.style.flexBasis = `calc(100% / ${size})`; 
         square.addEventListener('mouseenter',(e)=>{
-        e.target.style.backgroundColor="#0E1848";
+        let interactions = parseInt(e.target.dataset.interactions);
+        if (interactions === 0) {
+            // Pick a random index (0, 1, or 2) and apply the color
+            const randomNum = Math.floor(Math.random() * colorArray.length);
+            e.target.style.backgroundColor = colorArray[randomNum];
+            
+            // Start the opacity at 10% (0.1) and update the memory to 1
+            e.target.style.opacity = 0.1;
+            e.target.dataset.interactions = 1;
+        }
+        else if (interactions < 10) {
+        // Increase the count by 1
+        interactions++;
+        
+        // Calculate new opacity (e.g., 2 becomes 0.2, 5 becomes 0.5)
+        e.target.style.opacity = interactions / 10;
+        
+        // Save the new count back to the square's memory
+        e.target.dataset.interactions = interactions;
+        }
        })
         container.append(square);
     }
